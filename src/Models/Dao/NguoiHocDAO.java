eb5d4eb352cd32238fb983a5b16e8a4c53fde3c1
you have to better <you have to better@DESKTOP-S0UD02I>
2021-10-06 09:21:47 +0700

    Du An Mau da xong phan QLNV va QLCD

--- src/Models/Dao/NguoiHocDAO.java
+++ src/Models/Dao/NguoiHocDAO.java
@@ -5,10 +5,43 @@
  */
 package Models.Dao;
 
+import Entities.NguoiHoc;
+import java.util.List;
+
 /**
  *
  * @author you have to better
  */
-public class NguoiHocDAO {
+public class NguoiHocDAO extends EduSysDAO<NguoiHoc, String>{
+
+    @Override
+    public void insert(NguoiHoc entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(NguoiHoc entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public NguoiHoc selectById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<NguoiHoc> selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected List<NguoiHoc> selectBySql(String sql, Object... args) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
